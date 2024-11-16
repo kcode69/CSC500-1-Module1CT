e6a3c2bb12d584d392372eacc06c103defda9bb6
Kirk <kheck80@gmail.com>
2024-11-16 16:44:00 -0700

    First add, Part 1 add Part 2 multiply and devide
    CSC500-1 Module 1 Critical Thinking Part 1 (add & subtract) and Part 2 (multiply & divide) python code

---
+++ part1_add.py
@@ -0,0 +1,13 @@
+#input 2 numbers and output the sum of the 2 numbers
+#and output of num1 - num2
+
+print('Enter two numbers to be added and subtracted.')
+num1 = input('First number ')
+num2 = input('Second number ')
+
+sum = int(num1) + int(num2)
+sub = int(num1) - int(num2)
+print('The sum of ', num1, 'and ', num2, 'is ', sum)
+print(num1, 'minus ', num2,'is ', sub)
+
+
---
+++ part2_multiply_devide.py
@@ -0,0 +1,8 @@
+#input 2 numbers, and output the numbers multiplied and then divided
+
+print('Enter two numbers to be multiplied and divided.')
+num1 = input('First number ')
+num2 = input('Second number ')
+
+print(num1, 'multiplied by', num2, 'is', int(num1)*int(num2))
+print(num1, 'divided by', num2,'is', int(num1)/int(num2))
